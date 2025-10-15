6851a7ed98e9e647e344539f056e7e1c3d5533c7
divyanshu310102 <yadavdiyanshu489@gmail.com>
2025-10-15 14:15:23 +0530

    Optimized versions....07_08..

--- patternProblems/07.py
+++ patternProblems/07.py
@@ -7,10 +7,14 @@
 
 n = int(input('Enter no.: '))
 
-for i in range(n):
-    for j in range(n+i):
-        if i+j >= n-1:
-            print('*', end=" ")
-        else:
-            print(" ", end=" ")
-    print()            
+# for i in range(n):
+#     for j in range(n+i):
+#         if i+j >= n-1:
+#             print('*', end=" ")
+#         else:
+#             print(" ", end=" ")
+#     print()      
+#
+ #optimized version O(n)
+for i in range(1,n+1):
+    print((" " * (n - i)) + ('*' * (2*i-1)))
--- patternProblems/08.py
+++ patternProblems/08.py
@@ -6,10 +6,14 @@
 
 
 n = int(input('Enter no.: '))
-for i in range(n):
-    for j in range(2*n - i - 1):
-        if i > j:
-            print(" ", end=" ")
-        else:
-            print('*',end=" ")    
-    print()    
+# for i in range(n):
+#     for j in range(2*n - i - 1):
+#         if i > j:
+#             print(" ", end=" ")
+#         else:
+#             print('*',end=" ")    
+#     print()    
+
+ #Optimized version O(n)
+for i in range(n, 0, -1):
+    print(" " * (n - i) + (('*') * (2*i - 1)))
